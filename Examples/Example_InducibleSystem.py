5b3e714f7eca57e29b8316a468cd436dd5664927
JingWui Yeoh <yeohjingwui@gmail.com>
2019-05-03 17:01:51 +0800

    Updated Example python files

--- Examples/Example_InducibleSystem.py
+++ Examples/Example_InducibleSystem.py
@@ -23,31 +23,45 @@ def main():
     
     # Input_filename: The input data file in .csv following the sequences below:
     # 
-    # NOT gate: [Time(min) | state=0 | state=1 | SDstate=0 | SDstate=1]
-    # AND gate: [Time(min) | state=00 | state=01 | state=10 | state=11 | SDstate=00 | SDstate=01 | SDstate=10 | SDstate=11]
-    # OR gate: [Time(min) | state=00 | state=01 | state=10 | state=11 | SDstate=00 | SDstate=01 | SDstate=10 | SDstate=11]
+    # e.g.: [Time(min) | 100uM | 50uM | 10uM | 1uM | 0uM | SD100uM | SD50uM | SD10uM | SD1uM | SD0uM ]
+    # 
+    # The inducer concentration in the input file must be in units of %, uM, or nM; 
+    # Note: No spacing in word between concentration value and unit
+    # Note: The concentrations of inducer shall be in descending order. 
     # 
     # SD refers to the standard deviation data
     #
     # NumDataSet: Number of Data Set (excluding standard deviations)
     # Inducer_unit: The unit for the inducer concentration
     # OptInhibition: True if there is Inhibitory effect at high concentrations,
-    #                else None if there is only inducible trend
+    #           else None if there is only inducible trend
     # SystemOpt: Option for Inducible system ('ALL', 'ConstInd', 'DegradeInd', 'DelayInd', 'Inhibition')   
     # 
+    # 'ALL': (default setting) run through all the models available in the library
+    # 'ConstInd': run models under the assumption of constant inducer concentration
+    # 'DegradeInd': run models under the assumption of inducer with fast degradation behavior
+    # 'DelayInd': run models under the assumption of significant initial delayed response
+    # 'Inhibition': run models under the assumption of inhibition at high inducer concentrations
+    #
+    # Note: When the Inducer_unit is in '%', which refers to mass concentration in (g/100 mL), 
+    # users will be promted to insert the Inducer Molar Mass (in g/mol) at the console the unit 
+    # to convert to the unit of Molar.
+    #
     # Note: the SystemOpt is case-insensitive!
     #############################################################
     
     # Example file
-#    Input_filename = 'Inducible_pBADAra_rbspBb_MG.csv' # Arabinose (Data set 1)
-#    NumDataSet = 9
-#    Inducer_unit = '%' # Enter only (10**-6, 10**-9 or '%')
-#    OptInhibition = None
-    
-    Input_filename = 'pLac_IPTG_E6k.csv' 
+    Input_filename = 'Inducible_pBADAra_rbspBb_MG.csv' # Arabinose (Data set 1)
     NumDataSet = 9
-    Inducer_unit = 10**-6 # Enter only (10**-6, 10**-9 or '%')
+    Inducer_unit = '%' # Enter only (10**-6, 10**-9 or '%')
     OptInhibition = None
+    SystemOpt = 'ConstInd'
+    
+#    Input_filename = 'pLac_IPTG_E6k.csv' 
+#    NumDataSet = 9
+#    Inducer_unit = 10**-6 # Enter only (10**-6, 10**-9 or '%')
+#    OptInhibition = None
+#    SystemOpt = 'DelayInd'
     
 
     
@@ -62,7 +76,7 @@ def main():
     
 #    IS.DataReader(Inputfile, NumDataSet, Inducer_unit)
 #    IS.RunDoseResponsePrefitting(OptInhibition)
-#    IS.RunModels()
+#    IS.RunModels(SystemOpt)
 #    IS.RunModelSelection()
 #    IS.CreateOutputTextFile()
 #    IS.ExportModelDataFile()
@@ -73,7 +87,7 @@ def main():
     # Or use Helper function to autorun all functions
     #############################################################
     
-    IS.AutoRunInducibleSystem(Inputfile, NumDataSet, Inducer_unit, OptInhibition)
+    IS.AutoRunInducibleSystem(Inputfile, NumDataSet, Inducer_unit, OptInhibition, SystemOpt)
         
     sys.path.remove('../BMSSlibmod') #remove the path
     del IS
